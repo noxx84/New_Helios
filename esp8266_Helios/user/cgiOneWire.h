0302ab4b45b9f505e10ded478f026f0979698820
noxx84 <marek.szczypta@live.com>
2015-09-09 14:32:49 +0200

    Update cgiOneWire.h

--- esp8266_Helios/user/cgiOneWire.h
+++ esp8266_Helios/user/cgiOneWire.h
@@ -8,5 +8,9 @@
 #ifndef CGIONEWIRE_H_
 #define CGIONEWIRE_H_
 
+void  cgiOneWire(HttpdConnData *connData);
+void  cgiOneWire_cfg(HttpdConnData *connData);
+void  cgiOneWireManual(HttpdConnData *connData);
+void  cgiOneWire_read_temp(HttpdConnData *connData);
 
 #endif /* CGIONEWIRE_H_ */
